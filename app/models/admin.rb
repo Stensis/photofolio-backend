ec43cb6a56dcf9b0df7470dc9513b9a1cd0e7f63
Stensis <irenenjuguna98@gmail.com>
2022-10-11 11:22:03 +0300

    added booking model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,4 @@
 class Admin < ApplicationRecord
     has_one :gallery
-    has_many :users
-    has_many :users, through: :gallery
+    # has_many :users, through: :gallery
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,3 @@
-# class Booking < ApplicationRecord
-#     belongs_to :user
-# end
+class Booking < ApplicationRecord
+    belongs_to :user
+end
