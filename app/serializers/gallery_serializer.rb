e3185ceb06e0ec41efba22360895d63ab02b7b69
Stensis <irenenjuguna98@gmail.com>
2022-10-11 11:35:50 +0300

    added review serializer

--- app/serializers/gallery_serializer.rb
+++ app/serializers/gallery_serializer.rb
@@ -1,4 +1,4 @@
-# class GallerySerializer < ActiveModel::Serializer
-#   attributes :id, :category, :image, :title, :admin_id, :date
-#   has_many :reviews
-# end
+class GallerySerializer < ActiveModel::Serializer
+  attributes :id, :category, :image, :title, :admin_id, :date
+  has_many :reviews
+end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
-# class ReviewSerializer < ActiveModel::Serializer
-#   attributes :id, :gallery_id, :user_id, :review
-# end
+class ReviewSerializer < ActiveModel::Serializer
+  attributes :id, :gallery_id, :user_id, :review
+end
