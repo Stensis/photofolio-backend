fbb6a2afef696e53527ffe69cb2e2675e2d97386
Stensis <irenenjuguna98@gmail.com>
2022-10-11 11:07:43 +0300

    booking migration

--- db/migrate/20221009223954_create_bookings.rb
+++ db/migrate/20221009223954_create_bookings.rb
@@ -1,13 +1,13 @@
-# class CreateBookings < ActiveRecord::Migration[7.0]
-#   def change
-#     create_table :bookings do |t|
-#       t.string :username
-#       t.string :email
-#       t.string :session_type
-#       t.date :booking_date
-#       t.string :user_id
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.string :username
+      t.string :email
+      t.string :session_type
+      t.date :booking_date
+      t.string :user_id
 
-#       t.timestamps
-#     end
-#   end
-# end
+      t.timestamps
+    end
+  end
+end
