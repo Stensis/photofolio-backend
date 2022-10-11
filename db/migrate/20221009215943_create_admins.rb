3d441964337203d9fd97c2c30faf455191c66b21
Stensis <irenenjuguna98@gmail.com>
2022-10-11 11:08:08 +0300

    admins migration

--- db/migrate/20221009215943_create_admins.rb
+++ db/migrate/20221009215943_create_admins.rb
@@ -1,12 +1,12 @@
-# class CreateAdmins < ActiveRecord::Migration[7.0]
-#   def change
-#     create_table :admins do |t|
-#       t.string :username
-#       t.string :email
-#       t.string :password_digest
-#       t.integer :user_id
+class CreateAdmins < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admins do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.integer :user_id
 
-#       t.timestamps
-#     end
-#   end
-# end
+      t.timestamps
+    end
+  end
+end
