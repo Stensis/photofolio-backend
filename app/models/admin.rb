c5567df2340a1e1fc50830ef9dc225af58744e68
Stensis <irenenjuguna98@gmail.com>
2022-10-11 20:03:13 +0300

    completed

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ApplicationRecord
     has_one :gallery
     #validations
-    validates :name, presence: true
+    # validates :name, presence: true
     # has_many :users, through: :gallery
 end
