9a65aa319c9e5cad9358cc6163f7f39707c6ae26
Stensis <irenenjuguna98@gmail.com>
2022-10-12 21:52:07 +0300

    added deleting booking

--- config/application.rb
+++ config/application.rb
@@ -34,13 +34,13 @@ module Photofolio
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
 
     # handle cors for origin
     config.middleware.insert_before 0, Rack::Cors do
       allow do
          origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :options]
+         resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
        end
     end
   end
