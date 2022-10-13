3afa17f8d3737903d3bc1b0c4348ceff8ea57830
Stensis <irenenjuguna98@gmail.com>
2022-10-13 22:16:08 +0300

    sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,14 @@ class UsersController < ApplicationController
 
     # GET /user
     def index
-      users = User.all
-      render json: users
+      user = User.find_by(id: session[:user_id])
+      if user
+        users = User.all
+        render json: users
+      else
+        render json: { error: "Not authorized" }, status: :unauthorized
+      end
+    
     end
   
     # POST /user
