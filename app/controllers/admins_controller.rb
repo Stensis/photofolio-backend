b9f23396d11fd547a320daf07b0a3980075fec3a
Stensis <irenenjuguna98@gmail.com>
2022-10-14 04:59:48 +0300

    admins sessions

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,6 @@
 class AdminsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    skip_before_action :verify_authenticity_token
 
         # GET /admin
           def index
