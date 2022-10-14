9bbd6117bca8a9d5252ddca6ce48286cbabdf4b4
Stensis <irenenjuguna98@gmail.com>
2022-10-14 04:10:02 +0300

    admins sessions

--- app/controllers/admins_sessions_controller.rb
+++ app/controllers/admins_sessions_controller.rb
@@ -1,5 +1,6 @@
 class AdminsSessionsController < ApplicationController
     before_action :authorize
+    protect_from_forgery with: :null_session, only: [:create]
     skip_before_action :verify_authenticity_token
     skip_before_action :authorize, only: [:create]
 
